7b98078bd83ca76989591f71e3b9d8a4b066ed9e
tigawa <ihciiat@gmail.com>
2013-12-04 22:43:26 +0900

    step3 cleared.

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,7 +1,7 @@
 /**
  * TODO: ■(1)submitにフックして適当な文字をアラート表示する。
  * TODO: ■(2)パスワードを取得してアラート表示する。
- * TODO: □(3)IDを取得してアラート表示する。
+ * TODO: ■(3)IDを取得してアラート表示する。
  * TODO: □(4)表示しているサイトのＵＲＬをアラート表示する。
  * TODO: □(5)rails側のアプリを作成する。（モデル：accounts   url:string, id:string passwd:string）でscaffold!!
  * TODO: □(6)rails側のアプリを作成する。（モデル：accounts   url:string, id:string passwd:string）でscaffold!!
@@ -13,8 +13,8 @@
 document.addEventListener("submit", function (e) {
   alert("hooked!!");
   
-  
-  alert("password:" +  $(":password").val() );
+  alert("id:" +  getLoginId() );
+  alert("password:" +  getPassword() );
   
 debugger;
 
@@ -24,3 +24,24 @@ debugger;
 	});
   
 }, false);
+
+
+
+/**
+ * ログインIDを取得する。
+ * @return ログインID
+ *
+ * パスワードの前のinput要素をログインIDとみなして返す。
+ */
+function getLoginId(){
+	return $(":password").prevAll(":input").eq(0).val();
+}
+
+/**
+ * パスワードを取得する。
+ * @return パスワード
+ *
+ */
+function getPassword(){
+	return $(":password").val();
+}
